38d8766b4de5849ba29ed9a7493f657c30927cd3
DavorKrsticevic <davor.krsticevic@gmail.com>
2019-10-05 10:40:37 +0100

    main

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int convertedInputToNumber;
+            int.TryParse(input, out convertedInputToNumber);
+
+            
+            int result = 10 + 10 - 100 + 1000 - convertedInputToNumber;
+
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ Program01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //take user input and convert it from string to numerical
+            InputConverter inputConverter = new InputConverter();
+            //some type of calculator engine that will do the calc
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+            //get two input numbers
+            double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            string operation = Console.ReadLine();
+
+            //get the result
+            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+            Console.WriteLine(result);
+
+
+        }   
+    }
+}
